7db9411b31b1bb34cee663b26c6ff52c0ba7a957
manankalra <manankalra29@gmail.com>
2018-08-13 14:12:26 +0530

    Use Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
 
+import reducer from './reducers'
+import { createStore } from 'redux';
+
+const initialState = { tech: 'React' };
+const store = createStore(reducer, initialState);
+
 class App extends Component {
-  state = {
-    tech: "React"
-  };
+  // no state
   render() {
-    return <HelloWorld tech={this.state.tech} />;
+    return <HelloWorld tech={store.getState().tech} />;
   }
 }
 
